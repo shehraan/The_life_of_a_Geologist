4f94dac6d873c7e70d5cc865d928465e84246996
SH-10KJ-876971 <9944975-SH-10KJ-876971@users.noreply.replit.com>
2024-01-14 11:06:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function getId(id) {
+  return document.getElementById(id)
+}
+function yearToSalary(year) {
+  if (year == 2005) {
+    return salary = 86382
+  }
+  if (year == 2006) {
+    return salary = 88926
+  }
+  if (year == 2007) {
+    return salary = 91131
+  }
+  if (year == 2008) {
+    return salary = 93918
+  }
+  if (year == 2009) {
+    return salary = 96819
+  }
+  if (year == 2010) {
+    return salary = 98565
+  }
+  if (year == 2011) {
+    return salary = 98798
+  }
+  if (year == 2012) {
+    return salary = 99999
+  }
+  if (year == 2013) {
+    return salary = 99069
+  }
+  if (year == 2014) {
+    return salary = 99887
+  }
+  if (year == 2015) {
+    return salary = 100245
+  }
+  if (year == 2016) {
+    return salary = 101036
+  }
+  if (year == 2017) {
+    return salary = 102997
+  }
+  if (year == 2018) {
+    return salary = 103281
+  }
+  if (year == 2019) {
+    return salary = 104445
+  }
+  if (year == 2020) {
+    return salary = 104395
+  }
+  if (year == 2021) {
+    return salary = 107017
+  }
+}
